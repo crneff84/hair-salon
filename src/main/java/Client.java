354ad109369f3355ee8a099f357192eddcd8254a
Chance Neff <crneff84@gmail.com>
2016-09-23 09:48:21 -0700

    add backend for client and testing

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,4 +7,61 @@ public class Client {
   private int id;
   private int stylistId;
 
+  public Client(String name, int stylistId) {
+    this.name = name;
+    this.stylistId = stylistId;
+  }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if(!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+      this.getId() == newClient.getId() &&
+      this.getStylistId() == newClient.getStylistId();
+    }
+  }
+
+  public static List<Client> all() {
+    String sql = "SELECT id, name, stylistId FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients (name, stylistId) VALUES (:name, :stylistId)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("stylistId", this.stylistId)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getStylistId() {
+    return stylistId;
+  }
+
+  public static Client find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients where id=:id";
+      Client client = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Client.class);
+    return client;
+    }
+  }
+
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -20,4 +20,73 @@ public class ClientTest {
     }
   }
 
+  @Test
+  public void Client_instantiatesCorrectly_true() {
+    Client testClient = new Client("George", 1);
+    assertTrue(testClient instanceof Client);
+  }
+
+  @Test
+  public void getName_instantiatesWithName_String() {
+    Client testClient = new Client("George", 1);
+    assertEquals("George", testClient.getName());
+  }
+
+  @Test
+  public void getId_instantiatesWithId_true() {
+    Client testClient = new Client("George", 1);
+    testClient.save();
+    assertTrue(testClient.getId() > 0);
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfClient_true() {
+    Client firstClient = new Client("George", 1);
+    firstClient.save();
+    Client secondClient = new Client ("Tom", 2);
+    secondClient.save();
+    assertTrue(Client.all().get(0).equals(firstClient));
+    assertTrue(Client.all().get(1).equals(secondClient));
+  }
+
+  @Test
+  public void find_returnsClientWithSameId_secondClient() {
+    Client firstClient = new Client("George", 1);
+    firstClient.save();
+    Client secondClient = new Client ("Tom", 2);
+    secondClient.save();
+    assertEquals(Client.find(secondClient.getId()), secondClient);
+  }
+
+  @Test
+  public void equals_returnsTrueIfNamesAreTheSame() {
+    Client firstClient = new Client("George", 1);
+    Client secondClient = new Client ("George", 1);
+    assertTrue(firstClient.equals(secondClient));
+  }
+
+  @Test
+  public void save_returnsTrueIfNamesAreTheSame() {
+    Client testClient = new Client("George", 1);
+    testClient.save();
+    assertTrue(Client.all().get(0).equals(testClient));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Client testClient = new Client("George", 1);
+    testClient.save();
+    Client savedClient = Client.all().get(0);
+    assertEquals(testClient.getId(), savedClient.getId());
+  }
+
+  @Test
+  public void save_savesStylistIdIntoDB_true() {
+    Stylist testStylist = new Stylist("Fred", "Curly hair", 1);
+    testStylist.save();
+    Client testClient = new Client("George", testStylist.getId());
+    testClient.save();
+    Client savedClient = Client.find(testClient.getId());
+    assertEquals(savedClient.getStylistId(), testStylist.getId());
+  }
 }
